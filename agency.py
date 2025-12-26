766968aa834b8b20562e212bd5b12cf5c5e537fd
Ibaad Tariq <theaiagenteer@gmail.com>
2025-12-26 20:24:55 +0530

    Change communication flows

--- agency.py
+++ agency.py
@@ -1,18 +1,18 @@
 from dotenv import load_dotenv
 from agency_swarm import Agency
-
+from agent2 import coding_agent as code
 from coding_agent import coding_agent
 
 import asyncio
 
 load_dotenv()
-
+agent2 = code()
 
 # do not remove this method, it is used in the main.py file to deploy the agency (it has to be a method)
 def create_agency(load_threads_callback=None):
     agency = Agency(
         coding_agent,
-        communication_flows=[],
+        communication_flows=[coding_agent, agent2],
         name="CodingAgency",
         shared_instructions="shared_instructions.md",
         load_threads_callback=load_threads_callback,
---
+++ agent2/coding_agent.py
@@ -0,0 +1,20 @@
+from agents import ModelSettings
+from agency_swarm import Agent, WebSearchTool
+from openai.types.shared import Reasoning
+from coding_agent.tools import apply_patch, shell_tool, OpenAIImageGenerationTool, update_plan
+
+coding_agent = Agent(
+    name="CodingAgent",
+    description="A gpt-5.1-based coding assistant template with no tools or instructions configured.",
+    instructions="./instructions.md",
+    model="gpt-5.1-codex",
+    
+    model_settings=ModelSettings(
+        reasoning=Reasoning(
+            effort="medium",
+            summary="auto",
+        ),
+    ),
+)
+
+
